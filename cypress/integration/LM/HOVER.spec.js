39bbc43095bffd614d685bc5fdd6d0255a0dc5ad
Alex <demilisher1@gmail.com>
2020-06-18 00:21:48 +0600

    init cypress

--- cypress/integration/LM/HOVER.spec.js
+++ cypress/integration/LM/HOVER.spec.js
@@ -4,7 +4,7 @@ describe('Test for LogOut', () => {
         cy.get('#ui-id-2 > li > span').contains('Women').parent().find('ul.level0').invoke('show');
         cy.get('#ui-id-9').parent().find('ul.level1').invoke('show');
         cy.get('#ui-id-15').click()
-
+//
     })
 
 })
